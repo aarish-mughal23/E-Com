2afc5c6db2e6287ec63d2288ac724a774a0cbd26
Aarish Mughal <aarishmughal23@gmail.com>
2024-07-03 13:33:38 +0500

    Testing some Index Page Changes

--- index.php
+++ index.php
@@ -3,9 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap 5 CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+    <!-- Some Cool Font Name: Google Fonts -->
+
     <title>Ecommerce Store PHP</title>
 </head>
 <body>
-    <h1>HELLOW</h1>
+<header>
+      <nav class="py-2 bg-body-tertiary border-bottom" id="navigationBar">
+        <div class="d-flex flex-wrap px-5">
+          <ul class="nav me-auto">
+            <li class="nav-item">
+              <a href="#main" class="nav-link link-body-emphasis px-2 active" aria-current="page">Home</a>
+            </li>
+            <li class="nav-item">
+              <a href="#features" class="nav-link link-body-emphasis px-2">Item2</a>
+            </li>
+            <li class="nav-item">
+              <a href="#pricing" class="nav-link link-body-emphasis px-2">Item3</a>
+            </li>
+            <li class="nav-item">
+              <a href="#app" class="nav-link link-body-emphasis px-2">Item4</a>
+            </li>
+            <li class="nav-item">
+              <a href="#about" class="nav-link link-body-emphasis px-2">Item5</a>
+            </li>
+          </ul>
+          <ul class="nav">
+            <li class="nav-item pe-2">
+              <a href="./login.php" class="btn btn-outline-primary px-2">Login</a>
+            </li>
+            <li class="nav-item">
+              <a href="./signup.php" class="btn btn-primary px-2">Sign up</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
 </body>
 </html>
