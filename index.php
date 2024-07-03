9cc379df0a56cb5b4739e14c31865a5e69ccb3f5
Aarish Mughal <aarishmughal23@gmail.com>
2024-07-03 13:19:19 +0500

    Initial commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ECOM</title>
+    <title>Ecommerce Store PHP</title>
 </head>
 <body>
     <h1>HELLOW</h1>
